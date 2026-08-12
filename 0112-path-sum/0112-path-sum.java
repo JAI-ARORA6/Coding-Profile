6d8eb9388ad6320c620109147463c59b7e3a2b23
JAI-ARORA6 <156849159+JAI-ARORA6@users.noreply.github.com>
2026-08-13 03:08:50 +0530

    Time: 0 ms (100%), Space: 44.9 MB (50.69%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean res=false;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        int sum=0;
+        fun(root,sum,targetSum);
+        return res;
+    }
+
+    public void fun(TreeNode root,int sum,int targetSum){
+        if(root==null){
+            return ;
+        }
+
+        sum+=root.val;
+
+        if(root.left==null && root.right==null){
+            if(sum==targetSum){
+                res=true;
+            }
+            return ;
+        }
+
+        fun(root.left,sum,targetSum);
+        fun(root.right,sum,targetSum);
+
+        return ;
+    }
+}
