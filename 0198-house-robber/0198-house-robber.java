c992c6ee8833783816c471038d43c7bd2cf6470f
JAI-ARORA6 <156849159+JAI-ARORA6@users.noreply.github.com>
2026-08-30 22:01:09 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,37 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        ArrayList<ArrayList<Integer>> dp=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            ArrayList<Integer> t=new ArrayList<>();
-           t.add(-1); 
-            t.add(-1); 
-            dp.add(t);
+         if(nums.length==0) return 0;
+         int prev2=0;
+         int prev=nums[0];
+         for(int i=1;i<nums.length;i++){
+            int include=nums[i]+prev2;
+            int exclude=prev;
+            int curr=Math.max(include,exclude);
 
-        }
-        return fun(nums,n,0,1,dp);
+            prev2=prev;
+            prev=curr;
+         }
+         return prev;
     }
-
-    public int fun(int[] nums,int n,int i,int free,ArrayList<ArrayList<Integer>> dp){
-        if(i==n){
-            return 0;
-
-        }
-        if(dp.get(i).get(free)!=-1){
-            return dp.get(i).get(free);
-        }
-
-        if(free==0){
-            int ans=fun(nums,n,i+1,1,dp);
-            dp.get(i).set(free,ans);
-            
-            return ans;
-        }
-        int c1=nums[i]+fun(nums,n,i+1,0,dp);
-        int c2=fun(nums,n,i+1,1,dp);
-        int max=Math.max(c1,c2);
-        dp.get(i).set(free,max);
-        return max;
-    }
-
 }
