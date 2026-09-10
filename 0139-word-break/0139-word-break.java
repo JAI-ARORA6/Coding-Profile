aed94c772c2afb1f5e0ea5c2b56cfed1bf941083
JAI-ARORA6 <156849159+JAI-ARORA6@users.noreply.github.com>
2026-09-10 15:43:49 +0530

    Time: 9 ms (24.7%), Space: 46.4 MB (22.43%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> m=new HashSet<>(wordDict);
+        dp=new HashMap<>();
+        return fun(s,m);
+    }
+private Map<String,Boolean> dp;
+    public boolean fun(String s,Set<String> m){
+        if(s.length()==0){
+            return true;
+        }
+        if(dp.containsKey(s)){
+            return dp.get(s);
+        }
+        for(int i=0;i<s.length();i++){
+            String r=s.substring(0,i+1);
+            if(m.contains(r)){
+                if(fun(s.substring(i+1),m)){
+                    dp.put(s,true);
+                    return true;
+                }
+            }
+        }
+        dp.put(s,false);
+        return false;
+
+    }
+
+}
