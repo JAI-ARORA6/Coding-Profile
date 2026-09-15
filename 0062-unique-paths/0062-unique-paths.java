5f2931fcfa9e95199205cff7673f0a3e1acb1a3e
JAI-ARORA6 <156849159+JAI-ARORA6@users.noreply.github.com>
2026-09-15 17:37:35 +0530

    Time: 1 ms (13.4%), Space: 42.2 MB (47.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,31 +1,26 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
+       
         for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<n;j++){
+                int up=0;
+                int left=0;
+                if(i==0 && j==0){
+                    dp[0][0]=1;
+                }
+                
+                else{
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+                
+            }
         }
         
-        return fun(0,0,m,n,dp);
+        return dp[m-1][n-1];
         }
 
-        public int fun(int x,int y,int m,int n,int[][] dp){
-            if(x==m-1 && y==n-1){
-                return 1;
-            }
-            if(dp[x][y]!=-1){
-                return dp[x][y];
-            }
-
-            int right=0;
-            int down=0;
-
-            if(x<m-1){
-                down=fun(x+1,y,m,n,dp);
-            }
-            if(y<n-1){
-                right=fun(x,y+1,m,n,dp);
-            }
-
-            return dp[x][y]=right+down;
-        }
+   
 }
