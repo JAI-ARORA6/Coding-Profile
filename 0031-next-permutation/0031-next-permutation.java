776419ab4e24cd710e2c81ad72d1b89e40f55208
JAI-ARORA6 <156849159+JAI-ARORA6@users.noreply.github.com>
2026-09-18 02:16:04 +0530

    Time: 0 ms (100%), Space: 44.8 MB (15.8%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int index=-1;
+        int n=nums.length;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index!=-1){
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[index]){
+                swap(nums,i,index);
+                break;
+            }
+        }
+        reverse(nums,index+1,n-1);
+        }
+        else{
+            reverse(nums,0,n-1);
+        }
+
+    }
+
+    public void reverse(int[] nums,int start,int end){
+        while(start<end){
+            swap(nums,start,end);
+            start++;
+            end--;
+        }
+    }
+
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
