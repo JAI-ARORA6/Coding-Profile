3d5858703852600f807d83926898a55c77bf8bdc
JAI-ARORA6 <156849159+JAI-ARORA6@users.noreply.github.com>
2026-09-26 19:31:32 +0530

    Time: 1 ms (99.94%), Space: 94.2 MB (91.09%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,7 +3,7 @@ class Solution {
         int max=0;
         int min=Integer.MAX_VALUE;
         for(int i=0;i<prices.length;i++){
-            min=Math.min(prices[i],min);
+            min=Math.min(min,prices[i]);
             max=Math.max(max,prices[i]-min);
         }
         return max;
